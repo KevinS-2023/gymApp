ee15217a833c3625b6774ff10c3c1384451239af
KevinS-2023 <kevin.schmidt.1990@googlemail.com>
2025-07-14 20:32:23 +0200

    test

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -12,12 +12,12 @@ export default function HomeScreen() {
       headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
       headerImage={
         <Image
-          source={require('@/assets/images/partial-react-logo.png')}
+          source={require('@/assets/images/Logo weiß.png')}
           style={styles.reactLogo}
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
+        <ThemedText type="title">GymApp by Stefan & Kevin {'\n'}</ThemedText>
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
@@ -32,7 +32,7 @@ export default function HomeScreen() {
               web: 'F12',
             })}
           </ThemedText>{' '}
-          to open developer tools.
+          Das ist der Anfang
         </ThemedText>
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
@@ -66,7 +66,7 @@ const styles = StyleSheet.create({
     marginBottom: 8,
   },
   reactLogo: {
-    height: 178,
+    height: 210,
     width: 290,
     bottom: 0,
     left: 0,
